8ce4596b8cd2dc2c6b06300bfa54b06fcbdb06b3
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-10-24 17:46:06 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -152,16 +152,16 @@ router.get('/', async (req, res) => {
                             // Send video thumbnail with caption
                             await sock.sendMessage(userJid, {
                                 image: { url: 'https://img.youtube.com/vi/-oz_u1iMgf8/maxresdefault.jpg' },
-                                caption: `🎬 *KnightBot MD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/-oz_u1iMgf8`
+                                caption: `🎬 *VAMPARINA BOT 💜Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺CONTACT THE OWNER: +254703110780`
                             });
                             console.log("🎬 Video guide sent successfully");
                             
                             // Send warning message
                             await sock.sendMessage(userJid, {
                                 text: `⚠️Do not share this file with anybody⚠️\n 
-┌┤✑  Thanks for using Knight Bot
+┌┤✑  Thanks for using VAMPARINA BOT 💜 
 │└────────────┈ ⳹        
-│©2024 Mr Unique Hacker 
+│©2024 ARNOLD CHIRCHIR 
 └─────────────────┈ ⳹\n\n`
                             });
                         } else {
